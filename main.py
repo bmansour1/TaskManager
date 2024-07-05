e47d2387c71993598f69e70fe87f1874007fdd34
bmansour1 <117964164+bmansour1@users.noreply.github.com>
2024-07-04 21:06:26 -0400

    Initial project commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, url_for, flash, redirect, request
+import git
+from flask_behind_proxy import FlaskBehindProxy
+from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+import secrets
+import os
+
+
+# gets name of the .py file so Flask knows it's name
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = 'af6b94656bb3958cebd0b2e97ca9cba7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+
+
+# User Table represented as an object
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+    db.create_all()
+
+
+# tells you the URL the method below is related to
+@app.route("/")
+@app.route("/home")
+def hello_world():
+   # prints HTML to the webpage
+   return render_template('home.html', firstname = "Philana")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # add it to our User Table
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('hello_world'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/pabseodemo1/mysite/seo_demo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+
+if __name__ == '__main__':               # this should always be at the end
+    app.run(debug=True, host="0.0.0.0")
